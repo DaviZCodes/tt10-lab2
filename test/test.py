6c8ecf427f0c9e8f95cd5b0b1ae46c8a4547f46e
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2025-02-27 01:23:59 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -25,16 +25,48 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # Test 1 
+    dut.ui_in.value = 42
+    dut.uio_in.value = 241
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.uo_out.value == 13
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+    # Test 2 
+    dut.ui_in.value = 0
+    dut.uio_in.value = 1
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 0
+
+    # Test 3 
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 240
+
+    # Test 4 
+    dut.ui_in.value = 1
+    dut.uio_in.value = 0
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 8
+
+    # Test 5 
+    dut.ui_in.value = 1
+    dut.uio_in.value = 20
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 8
